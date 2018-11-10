2a6747a0499f1ceb1de3c5f50420d77af092a221
jianmo-he <jianmo.he@outlook.com>
2018-11-09 21:15:04 -0800

    comment out ml part for debugging

--- model.py
+++ model.py
@@ -61,51 +61,53 @@ for image, measurement in zip(images, measurements):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-input_shape=(160,320,3)
-num_classes=1
+print("x_train :",X_train.shape)
+print("y_train :",y_train.shape)
+# input_shape=(160,320,3)
+# num_classes=1
 
-# setup Keras model, normalize, and crop
-model = Sequential()
-model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=input_shape))
-model.add(Cropping2D(cropping=((70,25),(0,0))))
+# # setup Keras model, normalization, and cropping
+# model = Sequential()
+# model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=input_shape))
+# model.add(Cropping2D(cropping=((70,25),(0,0))))
 
-# LeNet model
-# model.add(Convolution2D(6,5,5,activation='relu'))
-# model.add(MaxPooling2D())
-# model.add(Convolution2D(6,5, 5, activation='relu'))
-# model.add(MaxPooling2D())
+# # LeNet model
+# # model.add(Convolution2D(6,5,5,activation='relu'))
+# # model.add(MaxPooling2D())
+# # model.add(Convolution2D(6,5, 5, activation='relu'))
+# # model.add(MaxPooling2D())
+# # model.add(Flatten())
+# # model.add(Dense(120))
+# # model.add(Dense(84))
+# # model.add(Dense(num_classes))
+
+# # Nvidia model
+# model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
+# model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+# model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+# model.add(Convolution2D(64,3,3,activation='relu'))
+# model.add(Convolution2D(64,3,3,activation='relu'))
 # model.add(Flatten())
-# model.add(Dense(120))
-# model.add(Dense(84))
+# model.add(Dense(100))
+# model.add(Dense(50))
+# model.add(Dense(10))
 # model.add(Dense(num_classes))
 
-# Nvidia model
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(64,3,3,activation='relu'))
-model.add(Convolution2D(64,3,3,activation='relu'))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(num_classes))
+# # compile model, train, and save the training history
+# model.compile(loss='mse', optimizer='adam')
+# history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=50)
 
-# compile model, train, and save the training history
-model.compile(loss='mse', optimizer='adam')
-history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs)
+# model.save('model.h5')
 
-model.save('model.h5')
+# ### print the keys contained in the history object
+# print(history_object.history.keys())
 
-### print the keys contained in the history object
-print(history_object.history.keys())
-
-### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
-savefig('model.png')
+# ### plot the training and validation loss for each epoch
+# plt.plot(history_object.history['loss'])
+# plt.plot(history_object.history['val_loss'])
+# plt.title('model mean squared error loss')
+# plt.ylabel('mean squared error loss')
+# plt.xlabel('epoch')
+# plt.legend(['training set', 'validation set'], loc='upper right')
+# plt.show()
+# savefig('model.png')
